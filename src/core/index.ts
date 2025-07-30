b5d3ad3a18ab2cc3fe42fef6aa465bc05868ab5d
Mr.Gr0wth <46140662+zaste@users.noreply.github.com>
2025-07-30 16:53:34 +0200

    feat: add core module barrel exports

---
+++ src/core/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Core module exports
+ */
+
+export { Scene } from './Scene';
+export { Camera } from './Camera';
+export { Renderer } from './Renderer';
+
+export type {
+  SceneConfig,
+  CameraConfig,
+  CameraTarget,
+  RendererConfig,
+  RenderStats,
+} from './Scene';
+export type {} from './Camera';
+export type {} from './Renderer';
